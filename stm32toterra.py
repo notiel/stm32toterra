57e1f48af6f7319782ebb1757e57f944853552cd
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-02-02 03:15:45 +0300

    +onelec parsing +delivery for terra catalogs

--- stm32toterra.py
+++ stm32toterra.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass, asdict
-from typing import Dict, List
+from typing import Dict, List, Union
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import sys
+import datetime
 
 from openpyxl import Workbook
 from openpyxl import load_workbook
@@ -10,16 +11,17 @@ from openpyxl.styles import Font, Alignment, Border, Side
 
 keys = ['Reference', 'Package', 'Flash', 'RAM']
 base_url = r'https://www.terraelectronica.ru'
+onelec_base = r'https://onelec.ru/products/'
 
 
 @dataclass
 class MicroController:
-    partnumber: str
-    is_available: bool
-    sales_data: list
-    package: str
-    ram: int
-    flash: int
+    partnumber: str      # code for cubemx
+    is_available: bool   # is available in terraelectronica
+    sales_data: list     # list of concrete pns, price, min quantity for order, instock, terra url, onelec url and price
+    package: str         # mc package
+    ram: int             # mc ram
+    flash: int           # mc flash
 
     def __str__(self):
         return str(asdict(self))
@@ -28,8 +30,8 @@ class MicroController:
 def get_start_index(sheet)-> int:
     """
     gets index for title row
-    :param sheet:
-    :return:
+    :param sheet: sheet excel with data
+    :return: index of table head row
     """
     for i in range(1, sheet.max_row):
         value = sheet['A%i' % i].value
@@ -57,14 +59,14 @@ def get_column_indexes(sheet, i: int) -> Dict[str, str]:
 
 def create_mc_list(sheet, indices: Dict[str, str], start_index: int) -> List[MicroController]:
     """
-
-    :param sheet:
-    :param indices:
-    :param start_index:
-    :return:
+    create mc list from excel mk data
+    :param sheet: excel shhet with data
+    :param indices: list of icolumn indiced
+    :param start_index: first row with data
+    :return: list of microcontrollers
     """
     microcontrollers: List[MicroController] = list()
-    for i in range(start_index+1, sheet.max_row):
+    for i in range(start_index+1, sheet.max_row+1):
         partnumber: str = sheet["%s%i" % (indices['Reference'], i)].value
         package: str = sheet["%s%i" % (indices['Package'], i)].value if 'Package' in indices.keys() else "Unknown"
         flash: str = sheet["%s%i" % (indices['Flash'], i)].value if 'Flash' in indices.keys() else "Unknown"
@@ -75,6 +77,39 @@ def create_mc_list(sheet, indices: Dict[str, str], start_index: int) -> List[Mic
     return microcontrollers
 
 
+def get_delivery_info(pn: str, product_id: str) -> List[List[Union[float, int]]]:
+    """
+    function gets delivery data for product
+    :param pn: product partnumber
+    :param product_id: id of product
+    :return: quantity available, number of delivery units, delivery unit: day or week, delivery prices
+    """
+    data = '{"jsonrpc":"2.0","method":"get_dms","params":{"code": %s, "name":"%s"}, "id": "objDMS||8"}'\
+           % (product_id, pn)
+    response = requests.post('https://www.terraelectronica.ru/services', data=data)
+    res = response.text
+    res = res.replace(r'\"', r'"')
+    res = res.replace("\n", "")
+    soup = BeautifulSoup(res)
+    delivery_data = soup.find_all('tr')[1:]
+    results = list()
+    for item in delivery_data:
+        try:
+            price = float(item.contents[1].contents[1].contents[1].attrs['data-price'])
+            count = int(item.contents[1].contents[1].contents[1].attrs['data-count'])
+            instock = int(item.contents[3].contents[1].text)
+            delivery = item.contents[5].contents[0]
+            delivery = delivery.replace('\\', "")
+            delivery = delivery.replace("n", "").strip()
+            if "дн" in delivery:
+                delivery = int(delivery.split()[0])
+                if delivery < 14:
+                    results.append([price, count, instock, delivery])
+        except (ValueError, TypeError):
+            print("Delivery exception")
+    return results
+
+
 def update_data_for_catalog(microcontroller: MicroController, request):
     """
     gets data for microcontroller for search redirected to catalog
@@ -82,15 +117,13 @@ def update_data_for_catalog(microcontroller: MicroController, request):
     :param request: request
     :return:
     """
-    url_present: str = request.url + '&f%5Bpresent%5D=1'
-    # print(url_present)
-    request = requests.get(url_present)
     soup = BeautifulSoup(request.text)
     items = soup.find_all('tr')
     results = list()
     for item in items[1:]:
         pn, price, count, product_id, instock = "", 0, 0, "", 0
         try:
+
             content_url = item.contents[3]
             if content_url.attrs['class'][0] == 'table-item-name':
                 content_url = content_url.contents[3]
@@ -103,11 +136,24 @@ def update_data_for_catalog(microcontroller: MicroController, request):
             content_count = item.contents[13].contents[1]
             if content_count.attrs['class'] == ['item-qnt']:
                 instock = content_count.contents[0].split()[0]
-            product_data = dict(PN=pn, Price=price, Count=count, Url=base_url + product_id, Instock=instock)
-            results.append(product_data)
-            print(product_id, pn, price, count, instock)
+            if int(instock) > 0:
+                product_data = dict(PN=pn, Price=price, Count=count, Url=base_url + product_id, Instock=instock, Days=0)
+                results.append(product_data)
+            else:
+                delivery_data = get_delivery_info(pn, product_id.split(r'/')[2])
+                for offer in delivery_data:
+                    product_data = dict(PN=pn, Price=offer[0], Count=offer[1], Url=base_url + product_id,
+                                        Instock=offer[2], Days=offer[3])
+                    results.append(product_data)
+
+            onelec_results = get_onelec_data(pn)
+            for result in onelec_results:
+                product_data = dict(PN=pn, Price=result[0], Count=result[3], Url=result[4], Instock=result[2],
+                                    Days=result[1])
+                results.append(product_data)
+
         except (TypeError, IndexError, KeyError):
-            print("error!")
+            print("error! in %s" % request.url)
             continue
     if len(results) > 0:
         microcontroller.sales_data = results.copy()
@@ -116,7 +162,7 @@ def update_data_for_catalog(microcontroller: MicroController, request):
 
 def update_from_common_catalog(microcontroller: MicroController, request):
     """
-
+     gets data from common catalog without filters
     :param microcontroller:
     :param request:
     :return:
@@ -141,16 +187,57 @@ def update_from_common_catalog(microcontroller: MicroController, request):
                 if content_count.attrs['class'] == ['item-qnt']:
                     instock = content_count.contents[0].split()[0]
             if int(instock) > 0:
-                product_data = dict(PN=pn, Price=price, Count=count, Url=base_url + product_id,
-                                    Instock=instock)
+                product_data = dict(PN=pn, Price=price, Count=count, Url=base_url + product_id, Instock=instock, Days=0)
                 results.append(product_data)
-                print(product_id, pn, price, count, instock)
+            else:
+                delivery_data = get_delivery_info(pn, product_id.split(r'/')[2])
+                for offer in delivery_data:
+                    product_data = dict(PN=pn, Price=offer[0], Count=offer[1], Url=base_url+product_id,
+                                        Instock=offer[2], Days=offer[3])
+                    results.append(product_data)
+            onelec_offers = get_onelec_data(pn)
+            for offer in onelec_offers:
+                product_data = dict(PN=pn, Price=offer[0], Count=offer[3], Url=offer[4], Instock=offer[2], Days=offer[1])
+                results.append(product_data)
+
         except (TypeError, IndexError, KeyError):
-            print("error!")
+            print("error! in %s" % request.url)
             continue
     if len(results) > 0:
         microcontroller.sales_data = results.copy()
         microcontroller.is_available = True
+
+
+def get_onelec_data(partnumber: str) -> List[List[Union[float, int, str]]]:
+    """
+    gets url and price from onelec
+    :param partnumber: partnumber of product
+    :return: price, url
+    """
+    url = onelec_base + partnumber.lower()
+    res = requests.get(url)
+    results = list()
+    if res.status_code != 404:
+        onelec_url = url
+        soup = BeautifulSoup(res.text)
+        table = soup.find('table', {'class': "table product-offers"})
+        try:
+            for tag in [tag for tag in table.contents[0].contents if isinstance(tag, Tag)]:
+                try:
+                    delivery = int(tag.contents[0].text.split()[1])
+                    if delivery <= 14 and 'по запросу' not in tag.contents[1].text:
+                        price = float(
+                            tag.contents[2].contents[0].contents[0].attrs['data-price-rub'].split()[0].replace(',', '.'))
+                        instock = int(tag.contents[0].contents[0].contents[4].text.split()[1])
+                        min_order = tag.contents[2].contents[1].text.split()[1]
+                        results.append([price, delivery, instock, min_order, onelec_url])
+                except ValueError:
+                    print("Onelec error at %s: " % onelec_url)
+                    print("Data got: ")
+                    print(results)
+        except AttributeError:
+            pass
+    return results
 
 
 def write_to_file(microcontrollers: List[MicroController]):
@@ -175,10 +262,12 @@ def write_to_file(microcontrollers: List[MicroController]):
     ws1['D1'] = 'Min count'
     ws1['E1'] = 'InStock'
     ws1['F1'] = 'Url'
-    ws1['G1'] = 'Package'
-    ws1['H1'] = 'Flash'
-    ws1['I1'] = 'RAM'
-    cells = [ws1['A1'], ws1['B1'], ws1['C1'], ws1['D1'], ws1['E1'], ws1['F1'], ws1['G1'], ws1['H1'], ws1['I1']]
+    ws1['G1'] = 'Days'
+    ws1['H1'] = 'Package'
+    ws1['I1'] = 'Flash'
+    ws1['J1'] = 'RAM'
+    cells = [ws1['A1'], ws1['B1'], ws1['C1'], ws1['D1'], ws1['E1'], ws1['F1'], ws1['G1'], ws1['H1'], ws1['I1'],
+             ws1['J1']]
     for cell in cells:
         cell.font = ft
         cell.border = border
@@ -193,12 +282,13 @@ def write_to_file(microcontrollers: List[MicroController]):
                 ws1['D%i' % i] = product['Count']
                 ws1['E%i' % i] = product['Instock']
                 ws1['F%i' % i] = product['Url']
-                ws1['G%i' % i] = microcontroller.package
-                ws1['H%i' % i] = microcontroller.flash
-                ws1['I%i' % i] = microcontroller.ram
+                ws1['G%i' % i] = product['Days']
+                ws1['H%i' % i] = microcontroller.package
+                ws1['I%i' % i] = microcontroller.flash
+                ws1['J%i' % i] = microcontroller.ram
                 i += 1
-    wb.save(filename='Results.xlsx')
-
+    now = datetime.datetime.now()
+    wb.save(filename='Results'+(str(now).split('.')[0].replace(":", "-"))+'.xlsx')
 
 
 def main(filename: str):
@@ -227,22 +317,23 @@ def main(filename: str):
         search_text = search_text.replace('x', '')
         url: str = base_url + r"/search?text=" + search_text
         r = requests.get(url)
+        print(r.url)
         if 'mikrokontrollery' in r.url:
             update_data_for_catalog(microcontroller, r)
         elif "search?" in r.url:
             soup = BeautifulSoup(r.text)
             links = soup.find('ul', {'class': "search-list"})
-            try:
-                link = base_url + '/' + links.contents[1].contents[1].attrs['href']
-            except (AttributeError, TypeError, IndexError):
-                print("Data error %s" % url)
-            r = requests.get(link)
-            update_data_for_catalog(microcontroller, r)
+            if links:
+                try:
+                    link = base_url + '/' + links.contents[1].contents[1].attrs['href']
+                    r = requests.get(link)
+                    update_data_for_catalog(microcontroller, r)
+                except (AttributeError, TypeError, IndexError):
+                    print("Data error %s" % url)
         elif r'catalog/products/' in r.url:
             update_from_common_catalog(microcontroller, r)
         elif r'/product/' in r.url:
-
-             pass
+            pass
         else:
             print(r.url+' is not parced')
 
@@ -254,4 +345,3 @@ if __name__ == '__main__':
         print("Please specify filename")
     else:
         main(sys.argv[1])
-
